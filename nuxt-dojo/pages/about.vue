e43ec272953d3bc133f5a674b6f174917d7574b8
Nikodimos-Developer <98909873+nikodimosewnetu@users.noreply.github.com>
2024-11-12 14:14:25 +0300

    Add files via upload

---
+++ nuxt-dojo/pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+<h2>About</h2>
+    
+<p>Lorem ipsum dolor sit amet consectetur
+   adipisicing elit. Esse error tenetur
+    perspiciatis voluptates aliquam porro 
+    asperiores doloremque ipsum consequatur 
+    reiciendis!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
---
+++ nuxt-dojo/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  
+  <h2>Home</h2>
+  <p>Lorem, ipsum dolor sit amet
+     consectetur adipisicing elit. 
+     Quae veniam quos dolore aliquam corrupti 
+    velit, odio rerum. Minus, aut cum.</p>
+  <p>Lorem, ipsum dolor sit amet
+     consectetur adipisicing elit. 
+     Quae veniam quos dolore aliquam corrupti 
+    velit, odio rerum. Minus, aut cum.</p>
+  <p>Lorem, ipsum dolor sit amet
+     consectetur adipisicing elit. 
+     Quae veniam quos dolore aliquam corrupti 
+    velit, odio rerum. Minus, aut cum.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
